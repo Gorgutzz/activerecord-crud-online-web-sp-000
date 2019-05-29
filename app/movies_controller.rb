89a8c3035ff86442957eb854b16a5e7e8e894d05
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-29 01:33:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,8 @@ def can_be_created_in_a_block(arg = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    __
+    m.title = arg[:title]
+    m.release_date = arg[:release_date]
   end
 end
 
